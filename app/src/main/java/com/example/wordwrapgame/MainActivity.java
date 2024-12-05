8ab4150cf5ee876a23a02bcd159e1b5ab428b546
ChristopheSan <chrisjakesanchez@gmail.com>
2024-12-04 17:28:39 -0800

    main activity

--- app/src/main/java/com/example/wordwrapgame/MainActivity.java
+++ app/src/main/java/com/example/wordwrapgame/MainActivity.java
@@ -1,11 +1,14 @@
 package com.example.wordwrapgame;
 
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.GradientDrawable;
 import android.os.Bundle;
 import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
@@ -14,10 +17,20 @@ public class MainActivity extends AppCompatActivity {
     private Button startGameButton;
     private Button statsButton;
 
+    private ConstraintLayout mainLayout;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mainLayout = findViewById(R.id.main);
+        GradientDrawable gradientDrawable = new GradientDrawable(
+                GradientDrawable.Orientation.TR_BL,
+                new int[]{Color.rgb(98, 160, 254), Color.rgb(145, 168, 250)}
+        );
+        mainLayout.setBackground(gradientDrawable);
+
 
         startGameButton = findViewById(R.id.startGameButton);
         statsButton = findViewById(R.id.gameStatsButton);
